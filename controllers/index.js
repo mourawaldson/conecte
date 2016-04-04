915f6bb61b925ccf184e180d61ab7b3da15d0f86
Waldson Moura <mourawaldson@Waldsons-MacBook-Pro.local>
2016-04-04 01:26:18 -0400

    testing

---
+++ controllers/index.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.use(require('./error'));
+
+//router.use('/comments', require('./comments'))
+
+module.exports = router;
+
+
+// 'use strict';
+
+// var IndexModel = require('../models/index');
+
+// module.exports = function (router) {
+//     var model = new IndexModel();
+
+//     router.get('/', function (req, res) {
+//         res.render('index', model);
+//     });
+// };
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+ 
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+     const numCPUs = require('os').cpus().length;
+
+     // Fork workers.
+     for (var i = 0; i < numCPUs; i++) {
+         cluster.fork();
+     }
+    
+     cluster.on('online', (worker, code, signal) => {
+         console.log(`worker ${worker.process.pid} is online`);
+     });
+    
+     cluster.on('exit', (worker, code, signal) => {
+         console.log(`worker ${worker.process.pid} died`);
+     });
+} else {
+    var express = require('express'),
+        app = express(),
+        bodyParser = require('body-parser');
+    const port = process.env.PORT || 3000;
+
+    // Setup views folder and view engine, making .ejs to .html
+    app.engine('.html', require('ejs').__express);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+
+    // Basic setup, express static folder (public), body parser and controllers folder
+    // public folder avaialbe as '/assets'
+    app.use('/assets', express.static(__dirname + '/public'));
+    app.use(bodyParser.json({type: '*/*'}));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(require('./controllers'));
+
+    // Let's start to listen...
+    app.listen(port, () => {
+        console.log('Listening on port ' + port);
+    });
+}
