3466ed39330ff78aa75411fa88bfe146a9809715
Waldson Moura <Waldson.Moura@mindgeekconsultants.com>
2017-11-03 02:59:27 -0300

    add about and contact pages, also update header menu navigation

--- routes/site.js
+++ routes/site.js
@@ -6,7 +6,24 @@ let express = require('express'),
 router.get('/', function(req, res) {
     res.render('index', {
         title: 'Conecte',
-        description: 'Conecte, sempre com você.'
+        description: 'Conecte, sempre com você.',
+        pageName: req.path
+    });
+});
+
+router.get('/about', function(req, res) {
+    res.render('about', {
+        title: 'Sobre',
+        description: 'Então...',
+        pageName: req.path
+    });
+});
+
+router.get('/contact', function(req, res) {
+    res.render('contact', {
+        title: 'Contato',
+        description: 'Fale conosco!',
+        pageName: req.path
     });
 });
 
